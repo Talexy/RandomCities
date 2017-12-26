97de087b0620d849df8a2c3ace7c199dc31e12c9
talexy <alexandru.tof@gmail.com>
2017-12-25 23:47:38 -0500

    local json

--- src/App.js
+++ src/App.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>Random Cities</h1>
+          <br/>
+          <ButtonCity />
+          <br/>
+          <br/>
+          <City />
+          <br/>
       </div>
     );
   }
 }
 
 export default App;
+
+
+export class ButtonCity extends Component {
+
+    render(){
+
+      return(
+        <button>Get Random City</button>
+
+      );
+
+    }
+}
+
+
+export class City extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        cityData:{}
+      }
+    }
+
+    getCityData(){
+      $.ajax({
+        url:'http://localhost:3000/cityData.json',
+        dataType:'json',
+        cache: false,
+        success: function(data){
+          this.setState({cityData: data});
+        }.bind(this),
+        error: function(xhr, status, err){
+          console.log(err);
+          alert(err);
+        }
+      });
+    }
+
+    componentDidMount(){
+      this.getCityData();
+    }
+
+    render(){
+        return (
+              <div>
+                <table>
+
+                    <tr><td>Ciy Name</td></tr>
+                    <tr><td>{this.state.cityData.name}</td></tr>
+                    <tr><td>Country</td></tr>
+                    <tr><td>{this.state.cityData.country}</td></tr>
+                    <tr><td>Ciy picture</td></tr>
+                    <tr><td>Continent</td></tr>
+                    <tr><td>Population</td></tr>
+                    <tr><td>Trip Advisor</td></tr>
+                    <tr><td>Fun facts</td></tr>
+                    <tr><td>Interesting Facts</td></tr>
+                    <tr><td>Pronounciation</td></tr>
+                    <tr><td>Fly there</td></tr>
+                    <tr><td>Carousel top things</td></tr>
+
+                </table>
+
+              </div>
+
+        );
+
+  }
+
+}
